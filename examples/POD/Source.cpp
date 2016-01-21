c2e9925d819d9948242a18cbf55d06fda9215bbe
chase <chase@4b9a4021-55c6-49ef-8927-9a4c88dedecd>
2016-01-21 16:23:43 +0000

    git-svn-id: https://lennon.ece.utexas.edu/ee380l.5/sp2016@8600 4b9a4021-55c6-49ef-8927-9a4c88dedecd

--- examples/POD/Source.cpp
+++ examples/POD/Source.cpp
@@ -4,14 +4,24 @@
 using std::cout;
 using std::endl;
 
-class Foo {
-	Foo(Foo &) {}
+class Complex {
+public:
+	double real;
+	double imag;
+
+	Complex(Complex const &) = default;
+	Complex(void) = default;
+	~Complex(void) = default;
+	Complex& operator=(Complex const &) = default;
 };
+
 
 int main(void) {
 	cout << "Hello World\n";
 
-	bool val = std::is_pod<Foo>::value;
+	bool val = std::is_pod<Complex>::value;
 	cout << std::boolalpha;
 	cout << val << endl;
+
+	Complex val1{ 1.0, 0.0 };
 }
