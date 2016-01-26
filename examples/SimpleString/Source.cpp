fc51d3fb20f0be9b2d629491991f5c65a9783f89
chase <chase@4b9a4021-55c6-49ef-8927-9a4c88dedecd>
2016-01-26 19:23:09 +0000

    git-svn-id: https://lennon.ece.utexas.edu/ee380l.5/sp2016@8675 4b9a4021-55c6-49ef-8927-9a4c88dedecd

--- examples/SimpleString/Source.cpp
+++ examples/SimpleString/Source.cpp
@@ -1,64 +1,25 @@
 #include <iostream>
 #include <cstdint>
 
+#include "String.h"
+
 using std::cout;
 using std::endl;
 
-class String {
-	char* data;
-	uint32_t length;
-public:
-	String(void) {
-		data = nullptr;
-		length = 0;
-	}
 
-	String(char const * src) {
-		length = 0;
-		while (src[length] != 0) {
-			length += 1;
-		}
-		if (length > 0) {
-			data = new char[length];
-			for (int k = 0; k < length; k += 1) {
-				data[k] = src[k];
-			}
-		}
-		else {
-			data = nullptr;
-		}
-	}
-
-	~String(void) { delete[] data; }
-
-	uint32_t size(void) const { return length; }
-
-	char& operator[](uint32_t k) { return data[k]; }
-	char operator[](uint32_t k) const { return data[k]; }
-
-	String& operator+=(String const& rhs) {
-		if (rhs.size() == 0) { return *this; } // special case for appending empty
-
-		char const* old_data = data;
-		uint32_t old_length = length;
-
-		length += rhs.length;
-		data = new char[length];
-		for (uint32_t k = 0; k < old_length; k += 1) {
-			data[k] = old_data[k];
-		}
-
-		for (uint32_t k = 0; k < rhs.length; k += 1) {
-			data[k + old_length] = rhs.data[k];
-		}
-
-		if (old_data) { delete[] old_data; }
-	}
-};
-
+void doit(String param) {
+	cout << param[1];
+}
 
 int main(void) {
 	String s1{ "Hello World" };
+
+	s1.operator[](0) = 'J';
+	s1[1] = 'a';
+
+	doit(s1);
+
+
 
 	for (uint32_t k = 0; k < s1.size(); k += 1) {
 		cout << s1[k];
