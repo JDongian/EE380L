4f363929313944da8cd9452fb187773a8c8b500f
chase <chase@4b9a4021-55c6-49ef-8927-9a4c88dedecd>
2016-02-16 15:17:42 +0000

    git-svn-id: https://lennon.ece.utexas.edu/ee380l.5/sp2016@12499 4b9a4021-55c6-49ef-8927-9a4c88dedecd

--- examples/Classes2/Source.cpp
+++ examples/Classes2/Source.cpp
@@ -5,22 +5,66 @@
 using std::cout;
 using std::endl;
 
-template <typename T>
-void doit(T const & x) {
+class Foo {};
+
+void doit(Foo const & x) {
 	cout << "lvalue fun\n";
 }
 
-template <typename T>
-void doit(T && x) {
+void doit(Foo  &&  x) {
 	cout << "rvalue fun\n";
 }
 
-class Foo {};
+
+void an_asside_rvalue_refs(void) {
+	Foo f;
+
+	using F = Foo const&&;
+	using T = std::remove_reference<F>::type;
+	if (std::is_const<T>::value) { cout << "it's a constant\n";  }
+	else { cout << "not a constant\n";  }
+	doit(static_cast<T&&>(f));
+
+
+	Foo const&& x = (Foo const&&)f;
+	doit((Foo&& )x);
+}
+
+class Shape {
+public:
+	void draw(void) { cout << "generic shape\n"; }
+
+};
+
+class Circle : public Shape {
+public:
+	int circularity; // stuff that makes a circle a circle (and not generic)
+	void draw(void) { cout << "circle\n"; }
+};
+
+class Square : public Shape {
+public:
+	int squareness; // stuff that makes a square a square 
+	void draw(void) { cout << "square\n"; }
+};
+
+void shapefun(Shape s) {
+	s.draw();
+}
 
 int main(void) {
-	Foo f;
-	Foo const&& y = (Foo const&&) f;
-	doit(y);
+//	an_asside_rvalue_refs();
 
+	Shape s;
+	Circle c;
+	Square r;
+
+	s.draw();
+	c.draw();
+	r.draw();
+
+	shapefun(s);
+	shapefun(c);
+	shapefun(r);
 }
 
