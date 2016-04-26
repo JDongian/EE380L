f8231de15060c25cf07bad8d42d839e98d42d689
chase <chase@4b9a4021-55c6-49ef-8927-9a4c88dedecd>
2016-04-26 16:54:53 +0000

    git-svn-id: https://lennon.ece.utexas.edu/ee380l.5/sp2016@16073 4b9a4021-55c6-49ef-8927-9a4c88dedecd

--- examples/MetaProgramming3/Source.cpp
+++ examples/MetaProgramming3/Source.cpp
@@ -32,6 +32,7 @@ template <> struct foo<false> {
 template <typename T, typename test=decltype(declval<T>()[0])>
 TRUE has_op_bracket(void) { return TRUE{}; }
 
+
 template <typename T, typename... not_used>
 FALSE has_op_bracket(not_used...) { return FALSE{}; }
 
@@ -44,10 +45,24 @@ void check_bracket(void) {
 	cout << eval<HasBracket<T>>() << endl;
 }
 
+struct Hack {
+	void foo(int) {}
+};
+
+template <typename T, typename test = decltype(declval<T>().foo(0))>
+TRUE has_foo(void) { return TRUE{}; }
+
+template <typename T, typename... not_used>
+FALSE has_foo(not_used...) { return FALSE{}; }
+
 void test1(void) {
-	check_bracket<int>();
-	check_bracket<int*>();
-	check_bracket<std::string>();
+	//has_op_bracket<int>();
+	//check_bracket<int>();
+	//check_bracket<int*>();
+	//check_bracket<std::string>();
+
+	cout << std::boolalpha;
+	cout << has_foo<Hack>().value << endl;
 }
 
 
@@ -73,20 +88,34 @@ struct Tuple<T, OtherArgs...> : public Tuple<OtherArgs...> {
 void test2(void) {
 	Tuple<int> x;
 	x.val = 42;
+
+	cout << x.val << endl;
+
 	Tuple<int, std::string> y;
 	y.val = 0;
+
 	Tuple<std::string>& y_tail = y;
 	y_tail.val = "Hello World";
+
+	Tuple<int, double, std::string> z;
+
 }
 
 template <int index, typename T>
 struct loop {
-	static constexpr int value = 1 + loop<index - 1, T>::value;
+	static constexpr int value = index * loop<index - 1, T>::value;
 };
 
 template <typename T> struct loop<0, T> {
-	static constexpr int value = 0;
+	static constexpr int value = 1;
 };
+
+int x[loop<6, int>::value];
+
+void test2point5(void) {
+	cout << loop<6, int>::value << endl;
+
+}
 
 template <typename T> struct tail_tuple;
 
@@ -117,11 +146,13 @@ int main(void) {
 
 	T x;
 
-	tuple_loop<0, T>::extract(x) = 42;
+	tuple_loop<0, T>::extract(x) = 14;
 	tuple_loop<1, T>::extract(x) = "hello world";
 
 	cout << tuple_loop<0, T>::extract(x) << endl;
 	cout << tuple_loop<1, T>::extract(x) << endl;
+
+	
 
 }
 
