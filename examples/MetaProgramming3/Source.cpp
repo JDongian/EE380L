e508c913cda04073f386e25c588b14accda6ca13
chase <chase@4b9a4021-55c6-49ef-8927-9a4c88dedecd>
2016-04-26 15:35:52 +0000

    git-svn-id: https://lennon.ece.utexas.edu/ee380l.5/sp2016@16071 4b9a4021-55c6-49ef-8927-9a4c88dedecd

--- examples/MetaProgramming3/Source.cpp
+++ examples/MetaProgramming3/Source.cpp
@@ -79,31 +79,49 @@ void test2(void) {
 	y_tail.val = "Hello World";
 }
 
-template <int index, typename tuple>
-struct get_component;
+template <int index, typename T>
+struct loop {
+	static constexpr int value = 1 + loop<index - 1, T>::value;
+};
 
-template <typename... tuple_args>
-struct get_component<0, Tuple<tuple_args...>> {
-	static auto extract(Tuple<tuple_args...>& tuple) -> decltype(tuple.val)& {
-		return tuple.val;
+template <typename T> struct loop<0, T> {
+	static constexpr int value = 0;
+};
+
+template <typename T> struct tail_tuple;
+
+template <typename T, typename... Tail> struct tail_tuple<Tuple<T, Tail...>> {
+	using type = Tuple<Tail...>;
+};
+
+template <int index, typename T>
+struct tuple_loop {
+	using short_tuple = typename tail_tuple<T>::type;
+	using type = typename tuple_loop < index - 1, short_tuple>::type;
+
+	static type& extract(T& arg) {
+		return tuple_loop<index - 1, short_tuple>::extract(arg);
 	}
 };
 
-template <int index, typename FirstArg, typename... tuple_args>
-struct get_component<index, Tuple<FirstArg, tuple_args...>> {
-	static auto extract(Tuple<FirstArg,tuple_args...>& tuple) 
-		-> decltype(get_component < index - 1, Tuple<tuple_args...>>(tuple))& {
-		return get_component < index - 1, Tuple<tuple_args...>>(tuple);
-	}
+template <typename T>
+struct tuple_loop<0, T> {
+	using type = decltype(declval<T>().val);
+	static type& extract(T& arg) { return arg.val; }
 };
+
 
 int main(void) {
 	using T = Tuple<int, std::string>;
+	using test = tuple_loop<0, T>::type;
+
 	T x;
-	get_component<0, T>::extract(x) = 42;
-	get_component<1, T>::extract(x) = "Hello World";
 
+	tuple_loop<0, T>::extract(x) = 42;
+	tuple_loop<1, T>::extract(x) = "hello world";
 
+	cout << tuple_loop<0, T>::extract(x) << endl;
+	cout << tuple_loop<1, T>::extract(x) << endl;
 
 }
 
