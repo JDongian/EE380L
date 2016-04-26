8bf4ccdae1ee29eb3e068418da4550fdd1139dea
chase <chase@4b9a4021-55c6-49ef-8927-9a4c88dedecd>
2016-04-26 15:01:32 +0000

    git-svn-id: https://lennon.ece.utexas.edu/ee380l.5/sp2016@16066 4b9a4021-55c6-49ef-8927-9a4c88dedecd

--- examples/MetaProgramming3/Source.cpp
+++ examples/MetaProgramming3/Source.cpp
@@ -5,6 +5,8 @@
 using std::cout;
 using std::endl;
 
+using std::declval;
+
 struct TRUE {
 	static constexpr bool value = true;
 };
@@ -16,6 +18,9 @@ struct FALSE {
 template <typename T>
 constexpr auto eval(T) -> decltype(T::value) { return T::value; }
 
+template <typename T>
+constexpr auto eval(void) -> decltype(T::value) { return T::value; }
+
 template <bool p> struct foo {
 	static std::string msg(void) { return "true"; }
 };
@@ -24,6 +29,55 @@ template <> struct foo<false> {
 	static std::string msg(void) { return "false"; }
 };
 
-int main(void) {
-	cout << foo<eval(FALSE{})>::msg() << endl;
+template <typename T, typename test=decltype(declval<T>()[0])>
+TRUE has_op_bracket(void) { return TRUE{}; }
+
+template <typename T, typename... not_used>
+FALSE has_op_bracket(not_used...) { return FALSE{}; }
+
+template <typename T>
+using HasBracket = decltype(has_op_bracket<T>());
+
+template <typename T>
+void check_bracket(void) {
+	cout << std::boolalpha;
+	cout << eval<HasBracket<T>>() << endl;
 }
+
+void test1(void) {
+	check_bracket<int>();
+	check_bracket<int*>();
+	check_bracket<std::string>();
+}
+
+
+
+
+
+/* -------------------------------------------------------------------- */
+
+template <typename... T>
+struct Tuple;
+
+template <typename T>
+struct Tuple<T> {
+	T val;
+};
+
+template <typename T, typename... OtherArgs>
+struct Tuple<T, OtherArgs> : public Tuple<OtherArgs...> {
+	T val;
+};
+
+
+int main(void) {
+	Tuple<int> x;
+	x.val = 42;
+	Tuple<int, int> y;
+}
+
+
+
+
+
+
